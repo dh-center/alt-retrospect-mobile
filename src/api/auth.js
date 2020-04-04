0f1e6bd8f8995d22b5db5f6fe667d30d1b7ef19a
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-04-04 22:07:01 +0300

    Basic file and dir structure

---
+++ src/api/auth.js
@@ -0,0 +1,13 @@
+function userSignUp(username, password) {
+    return {
+        email: '',
+        username: username,
+        id: 4,
+    };
+}
+
+function userSignIn(username, password) {
+    return {
+        auth_token: 'c89061a66e1241fda2fa870d22e5ecd7ebc29dfe',
+    };
+}
---
+++ src/api/profile.js
@@ -0,0 +1,7 @@
+function getUserInfo() {
+    return {
+        email: '',
+        id: 4,
+        username: 'username',
+    };
+}
