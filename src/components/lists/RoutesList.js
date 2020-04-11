1b798cfd4b3b96f2964fb6871acbf737e4b4f506
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-04-12 00:20:22 +0300

    #1: Added chevron to list items on RouteScreen

--- src/components/lists/RoutesList.js
+++ src/components/lists/RoutesList.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {Icon, ListItem} from '@ui-kitten/components';
+import {Icon, ListItem, Button} from '@ui-kitten/components';
+import {ArrowRightIcon} from '../icons/ArrowRightIcon';
 
 export const RoutesList = ({data}) => {
     const renderRouteIcon = style => <Icon {...style} name="person" />;
+    const renderChevron = style => (
+        <Button
+            appearance="ghost"
+            status="basic"
+            style={style}
+            icon={ArrowRightIcon}
+        />
+    );
 
     const renderItem = item => (
-        <ListItem key={item.id} title={item.name} icon={renderRouteIcon} />
+        <ListItem
+            key={item.id}
+            title={item.name}
+            icon={renderRouteIcon}
+            accessory={renderChevron}
+        />
     );
 
     return data.map(renderItem);
--- src/components/lists/TagsList.js
+++ src/components/lists/TagsList.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Icon, ListItem, useStyleSheet} from '@ui-kitten/components';
+import {Button, Icon, ListItem, useStyleSheet} from '@ui-kitten/components';
 import {tagStyles} from '../../styles/styleProvider';
+import {ArrowRightIcon} from '../icons/ArrowRightIcon';
 
 export const TagsList = ({data}) => {
     const styles = useStyleSheet(tagStyles);
 
     const renderTagIcon = style => <Icon {...style} name="person" />;
+    const renderChevron = style => (
+        <Button
+            appearance="ghost"
+            status="basic"
+            style={style}
+            icon={ArrowRightIcon}
+        />
+    );
 
     const renderItem = item => (
         <ListItem
             key={item.id}
             title={`${item.name}`}
             icon={renderTagIcon}
+            accessory={renderChevron}
             style={styles.tagBody}
         />
     );
