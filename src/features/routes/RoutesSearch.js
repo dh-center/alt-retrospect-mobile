5d6d6d8d0b0db58ed5eab63dacada819c82c34d3
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-05-01 12:09:37 +0300

    #1: Added noRoutes message and removed redundant rerendering on RoutesSearch screen

--- src/features/routes/RoutesSearch.js
+++ src/features/routes/RoutesSearch.js
@@ -1,46 +1,55 @@
 import React from 'react';
 import {ScrollView, StatusBar} from 'react-native';
-import {Layout, Spinner, useStyleSheet} from '@ui-kitten/components';
+import {Layout, Spinner, Text, useStyleSheet} from '@ui-kitten/components';
 import RoutesList from '../../components/lists/RoutesList';
 import {searchScreenStyles, sharedStyles} from '../../styles/styleProvider';
 import {fetchSearchRoutes} from '../../actions/routes';
 import {connect} from 'react-redux';
 import {SearchBar} from '../../components/inputs/SearchBar';
+import {str} from '../../i18n';
 
 const RoutesSearch = props => {
     const styles = useStyleSheet(searchScreenStyles);
     const shared = useStyleSheet(sharedStyles);
 
-    if (props.isFetching) {
-        return (
-            <Layout style={shared.centerContent}>
-                <Spinner />
-            </Layout>
-        );
-    } else {
-        return (
-            <Layout style={styles.flexArea} level="3">
-                <StatusBar
-                    backgroundColor={styles.statusBar.backgroundColor}
-                    barStyle="light-content"
+    return (
+        <Layout style={styles.flexArea} level="3">
+            <StatusBar
+                backgroundColor={styles.statusBar.backgroundColor}
+                barStyle="light-content"
+            />
+            <Layout style={styles.headerLayout} level="3">
+                <SearchBar
+                    style={styles.searchBar}
+                    onChangeText={text =>
+                        text !== '' ? props.fetchRoutes(text) : () => null
+                    }
                 />
-                <Layout style={styles.headerLayout} level="3">
-                    <SearchBar
-                        style={styles.searchBar}
-                        onChangeText={text => props.fetchRoutes(text)}
-                    />
-                </Layout>
-                <Layout style={styles.roundedLayout} level="1">
-                    <ScrollView contentContainerStyle={styles.scrollPadded}>
-                        <RoutesList
-                            data={props.routes}
-                            navigation={props.navigation}
-                        />
-                    </ScrollView>
-                </Layout>
             </Layout>
-        );
-    }
+            <Layout style={styles.roundedLayout} level="1">
+                {props.isFetching ? (
+                    <Layout style={shared.centerContent}>
+                        <Spinner />
+                    </Layout>
+                ) : (
+                    <ScrollView contentContainerStyle={styles.scrollPadded}>
+                        {props.routes.length === 0 ? (
+                            <Layout style={shared.centerContent}>
+                                <Text appearance="hint">
+                                    {str('routes.noRoutes')}
+                                </Text>
+                            </Layout>
+                        ) : (
+                            <RoutesList
+                                data={props.routes}
+                                navigation={props.navigation}
+                            />
+                        )}
+                    </ScrollView>
+                )}
+            </Layout>
+        </Layout>
+    );
 };
 
 const mapDispatchToProps = dispatch => {
