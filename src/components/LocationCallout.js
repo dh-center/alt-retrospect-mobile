a9061c30cc0db34d366e905e4767e77994f1042e
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-05-31 19:42:55 +0300

    #13: Refactored location callout styles

--- src/components/LocationCallout.js
+++ src/components/LocationCallout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {Button, Icon, useStyleSheet} from '@ui-kitten/components';
-import {calloutStyles} from '../styles/styleProvider';
+import {Button, Icon, StyleService, useStyleSheet} from '@ui-kitten/components';
+import {Alignment, Spacing, Colors} from '../styles';
 
 export const LocationCallout = props => {
     const styles = useStyleSheet(calloutStyles);
@@ -26,3 +26,40 @@ export const LocationCallout = props => {
         </View>
     );
 };
+
+// These styles are very component-specific,
+// so they reuse only a few common component styles
+const calloutStyles = StyleService.create({
+    containerView: {
+        ...Spacing.basePadding,
+        ...Alignment.row,
+        ...Alignment.roundedMedium,
+        ...Colors.whiteBackground,
+        width: 200,
+    },
+    contentView: {
+        width: '90%',
+        justifyContent: 'center',
+    },
+    locationTitle: {
+        fontSize: 16,
+    },
+    buttonView: {
+        width: '10%',
+    },
+    tooltip: {
+        width: 0,
+        height: 0,
+        backgroundColor: 'transparent',
+        borderStyle: 'solid',
+        borderTopWidth: 15,
+        borderRightWidth: 10,
+        borderBottomWidth: 0,
+        borderLeftWidth: 10,
+        borderTopColor: '#fff',
+        borderBottomColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderLeftColor: 'transparent',
+        left: 90,
+    },
+});
--- src/styles/styleProvider.js
+++ src/styles/styleProvider.js
@@ -15,32 +15,3 @@ export const sharedStyles = StyleService.create({
         justifyContent: 'center',
     },
 });
-
-export const calloutStyles = StyleService.create({
-    containerView: {
-        backgroundColor: '#fff',
-        width: 200,
-        padding: 15,
-        borderRadius: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    contentView: {width: '90%', justifyContent: 'center'},
-    locationTitle: {fontSize: 16},
-    buttonView: {width: '10%'},
-    tooltip: {
-        width: 0,
-        height: 0,
-        backgroundColor: 'transparent',
-        borderStyle: 'solid',
-        borderTopWidth: 15,
-        borderRightWidth: 10,
-        borderBottomWidth: 0,
-        borderLeftWidth: 10,
-        borderBottomColor: 'transparent',
-        borderRightColor: 'transparent',
-        borderTopColor: '#fff',
-        borderLeftColor: 'transparent',
-        left: 90,
-    },
-});
